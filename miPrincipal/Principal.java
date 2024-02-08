0756645299f96beaee565d697a54e7fcaa8944cb
BaltazarIT <159161138+BaltazarIT@users.noreply.github.com>
2024-02-08 08:17:46 -0700

    Update Principal.java

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -3,40 +3,57 @@ package miPrincipal;
 import java.util.Scanner;
 
 public class Principal {
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opc = 0;
         do {
-            System.out.println("TIPOS DE DATOS ABSTRACTOS
-            \n 1) Rational
-            \n 2) Matriz 
-            \n 3) Conjunto
-            \n 4) Persona
-            \n 5) Fecha
-            \n 6) Cadena 
-            \n 7) Numerote
-            \n 8) Cuenta cheques 
-            \n 9) Punto
-            \n 10) Triangulo
-            \n 0) Salir
-            \n >> ");
-            opc = sc.nextInt();
-            switch(opc) {
+            System.out.println("=========================");
+            System.out.println("TIPOS ABSTRACTOS DE DATOS");
+            System.out.println("=========================");
+            System.out.println("1) RATIONAL");
+            System.out.println("2) MATRIZ  ");
+            System.out.println("3) CONJUNTO");
+            System.out.println("4) PERSONA ");
+            System.out.println("5) FECHA   ");
+            System.out.println("6) CADENA  ");
+            System.out.println("7) NUMEROTE");
+            System.out.println("8) CUENTA_CHEQUES");
+            System.out.println("9) PUNTO");
+            System.out.println("10)TRIANGULO");
+            System.out.println("            ");
+            System.out.println("0) SALIR");
+            System.out.print("Selecciona opción:");
+            opc = consola.nextInt();
+
+            switch (opc) {
                 case 1:
-                    AppRational();
+                    AppRational.menu();
+                    break;
                 case 2:
+                    break;
                 case 3:
+                    break;
                 case 4:
+                    break;
                 case 5:
+                    break;
                 case 6:
+                    break;
                 case 7:
+                    break;
                 case 8:
+                    break;
                 case 9:
-                case 10:   
+                    break;
+                case 10:
+                    break;
+                case 0:
+                    System.out.println("...");
+                    consola.close();
+                    break;
                 default:
-                    System.out.println("Valor incorrecto, intenta de nuevo"); 
+                    System.out.println("Valor incorrecto, intente de nuevo!");
             }
-        } while(opc!=0);
+        } while (opc != 0);
     }
 }
